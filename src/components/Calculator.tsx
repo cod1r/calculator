07593c369f3414796ba4a968b8cc89b4c334f8ee
cod1r <jasonho353@gmail.com>
2020-12-23 03:35:39 -0600

    history functionality added

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,12 +1,14 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import ExpressionInput from "./ExpressionInput";
 import "./Calculator.css";
 
 
-// TODO: Make a compute component...maybe for "history" component
 function Calculator(){
     const [expression, setExp] = useState("");
     const [answer, setAns] = useState(0);
-    const evaluate = (expr : string) : void => {
+    const [history, setHist] : [Array<any>, any] = useState([]);
+    // TODO: improve evaluate function and test it for any bugs. Need to add exponents and parenthesis
+    const evaluate = (expr : string) : number => {
         let exp : string = "";
         for (let i = 0; i < expr.length; i++) {
             if (expr[i] === ' '){
@@ -16,24 +18,23 @@ function Calculator(){
         }
         let expression : Array<string> = [], operators : Array<string> = [], temp : string = "";
         for (let i = 0; i < exp.length; i++){
-            if ((exp[i] === '*' || exp[i] === '/') && operators.length > 0 && operators[operators.length-1] !== '('){
+            if (operators.length > 0 && (operators[operators.length-1] === '*' || operators[operators.length-1] === '/') && operators[operators.length-1] !== '('){
                 expression.push(temp);
                 let expr1 : string | undefined = expression.pop();
                 let expr2 : string | undefined = expression.pop();
-                let op : string = exp[i];
+                let op : string | undefined = operators.pop();
                 
                 if (expr1 !== undefined && expr2 !== undefined){
                     expression.push(expr1 + " " + expr2 + op);
                 }
                 operators.push(exp[i]);
                 temp = "";
-            } else if ((exp[i] === '+' || exp[i] === '-') && operators.length > 0 && operators[operators.length-1] !== '(') {
+            } else if (operators.length > 0 && (operators[operators.length-1] === '+' || operators[operators.length-1] === '-') && operators[operators.length-1] !== '(') {
                 expression.push(temp);
                 let expr1 : string | undefined = expression.pop();
                 let expr2 : string | undefined = expression.pop();
                 let op : string | undefined = operators.pop();
                 if (expr1 !== undefined && expr2 !== undefined){
-                    
                     expression.push(expr2 + " " + expr1 + op);
                 }
                 operators.push(exp[i]);
@@ -59,6 +60,7 @@ function Calculator(){
             }
         }
         let postfix : string | undefined = expression.pop();
+        console.log(postfix);
         let values : Array<number> = [], tmp : string = "";
         if (postfix !== undefined){
             for (let i = 0; i < postfix.length; i++){
@@ -84,37 +86,48 @@ function Calculator(){
                 }
             }
         }
-        setAns(values[0]);
+        
+        return values[0];
     }
-    const getExp = (e : any) : any => {
-        setExp(e.target.value);
-        evaluate(e.target.value);
+    const del = (i : number) : void => {
+        history.splice(i, 1);
+        setHist([...history]);
     }
     
     return (
         <div id="calc">
             <div id="type-area">
-                <input type="text" id="in" onInput={getExp} onChange={getExp} value={expression}></input>
+                <input type="text" id="in" onChange={(e) => {setExp(e.target.value); setAns(evaluate(e.target.value))}} value={expression} onKeyDown={(e) => { 
+                    if (e.key === "Enter") {
+                        let copy : Array<any> = [...history];
+                        copy.push("");
+                        setHist(copy);
+                }}}></input>
+                <div id="answer">{expression.length > 0 && !expression.match(/^[A-Za-z]+$/) && answer !== undefined ? '='+answer:""}</div>
             </div>
-            <div id="answer">{expression.length > 0 && !expression.match(/^[A-Za-z]+$/) ? answer:""}</div>
             <div id="operations">
                 <div id="numbers">
-                    <button onClick={() => {setExp(expression + 1); evaluate(expression + 1);}}>1</button>
-                    <button onClick={() => {setExp(expression + 2); evaluate(expression + 2);}}>2</button>
-                    <button onClick={() => {setExp(expression + 3); evaluate(expression + 3);}}>3</button>
-                    <button onClick={() => {setExp(expression + 4); evaluate(expression + 4);}}>4</button>
-                    <button onClick={() => {setExp(expression + 5); evaluate(expression + 5);}}>5</button>
-                    <button onClick={() => {setExp(expression + 6); evaluate(expression + 6);}}>6</button>
-                    <button onClick={() => {setExp(expression + 7); evaluate(expression + 7);}}>7</button>
-                    <button onClick={() => {setExp(expression + 8); evaluate(expression + 8);}}>8</button>
-                    <button onClick={() => {setExp(expression + 9); evaluate(expression + 9);}}>9</button>
+                    <button onClick={() => {setExp(expression + 1); setAns(evaluate(expression + 1));}}>1</button>
+                    <button onClick={() => {setExp(expression + 2); setAns(evaluate(expression + 2));}}>2</button>
+                    <button onClick={() => {setExp(expression + 3); setAns(evaluate(expression + 3));}}>3</button>
+                    <button onClick={() => {setExp(expression + 4); setAns(evaluate(expression + 4));}}>4</button>
+                    <button onClick={() => {setExp(expression + 5); setAns(evaluate(expression + 5));}}>5</button>
+                    <button onClick={() => {setExp(expression + 6); setAns(evaluate(expression + 6));}}>6</button>
+                    <button onClick={() => {setExp(expression + 7); setAns(evaluate(expression + 7));}}>7</button>
+                    <button onClick={() => {setExp(expression + 8); setAns(evaluate(expression + 8));}}>8</button>
+                    <button onClick={() => {setExp(expression + 9); setAns(evaluate(expression + 9));}}>9</button>
                 </div>
                 <div id="basic">
-                    <button onClick={() => {setExp(expression + ' + '); evaluate(expression + ' + ');}}>&#43;</button>
-                    <button onClick={() => {setExp(expression + ' - '); evaluate(expression + ' - ');}}>&#8722;</button>
-                    <button onClick={() => {setExp(expression + ' * '); evaluate(expression + ' * ');}}>&#215;</button>
-                    <button onClick={() => {setExp(expression + ' / '); evaluate(expression + ' / ');}}>&#247;</button>
+                    <button onClick={() => {setExp(expression + ' + '); setAns(evaluate(expression + ' + '));}}>&#43;</button>
+                    <button onClick={() => {setExp(expression + ' - '); setAns(evaluate(expression + ' - '));}}>&#8722;</button>
+                    <button onClick={() => {setExp(expression + ' * '); setAns(evaluate(expression + ' * '));}}>&#215;</button>
+                    <button onClick={() => {setExp(expression + ' / '); setAns(evaluate(expression + ' / '));}}>&#247;</button>
                 </div>
+            </div>
+            <div id="history">
+                {
+                    history.map((x, index) => <ExpressionInput key={index} evaluate={evaluate} del={del} index={index} expr={x} history={history}></ExpressionInput>)
+                }
             </div>
         </div>
     );

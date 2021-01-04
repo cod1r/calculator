7c4e439c2192c29d6e946fb3ae62320e59e3934b
cod1r <jasonho353@gmail.com>
2021-01-04 06:50:22 -0600

    added parentheses and exponents - some bugs are left

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,6 +1,29 @@
 import { useEffect, useRef, useState } from "react";
 import ExpressionInput from "./ExpressionInput";
 import "./Calculator.css";
+/*
+expression playground
+
+3*(3+4)
+
+4 3 + 3 *
+
+3*(3+4*3)
+
+3 3 4 3
+* ( + * )
+
+3*(3*4+3)
+
+3 ,3 4 *, 3
+* ( + )
+
+
+3*(3-3*6+15)
+
+3,3 3 6 *-, 15
+* ( + )
+*/
 
 function Calculator() {
   const [expression, setExp] : [string, any] = useState("");
@@ -8,7 +31,9 @@ function Calculator() {
   const [history, setHist] : [Array<any>, any] = useState([]);
   // TODO: improve evaluate function and test it for any bugs. Need to add exponents and parenthesis
   // TODO: account for postfix not evaluating right because of negative numbers ( we did but i just checked the next index to see if it was a number )
-  // TODO: adding parenthesis
+  // TODO: adding parenthesis - I think I am done. (maybe need to test)
+  // TODO: adding exponents - I think I am done. (maybe need to test)
+  // TODO: need to polish things up ( like when an operator is missing one of its parameters )
   const evaluate = (expr: string): number => {
     let exp: string = "";
     for (let i = 0; i < expr.length; i++) {
@@ -17,6 +42,28 @@ function Calculator() {
       }
       exp += expr[i];
     }
+    // if the expression isn't finished yet then return NaN
+    switch(exp[exp.length-1]){
+        case'^':
+        case'*':
+        case'/':
+        case'+':
+        case'-': return NaN;
+        default: break;
+    }
+    // just in case there are parentheses that don't match up.
+    let opening_p : Array<string> = [];
+    for (let i = 0; i < exp.length; i++){
+        if (exp[i] === '('){
+            opening_p.push(exp[i]);
+        }
+        else if (exp[i] === ')' && opening_p.length > 0 && opening_p[opening_p.length-1] === '('){
+            opening_p.pop();
+        }
+    }
+    // if there aren't matching parentheses, then return NaN.
+    if (opening_p.length > 0) return NaN;
+
     let expression: Array<string> = [],
         operators: Array<string> = [],
         temp: string = "";
@@ -24,12 +71,53 @@ function Calculator() {
     // ---------------------------------------------------------- BEGINNING OF POSTFIX CONVERSION
     for (let i = 0; i < exp.length; i++){
         console.log("Temp: ", temp);
-        if ((exp[i] === '*' || exp[i] === '/' || exp[i] === '+' || exp[i] === '-') && temp.length > 0){
+        if (exp[i] === '('){
+            operators.push(exp[i]);
+            continue;
+        }
+        if (exp[i] === ')'){
+            if (temp.length > 0){
+                expression.push(temp);
+                temp = "";
+            }
+            let one : string | undefined = expression.pop();
+            let two : string | undefined = expression.pop();
+            let op : string | undefined = operators.pop();
+            if (one !== undefined && two !== undefined && op !== undefined){
+                switch(op){
+                    case '^':
+                    case '+':
+                    case '-': expression.push(two + " " + one + op); break;
+                    case '*':
+                    case '/': expression.push(one + " " + two + op); break;
+                    default: break;
+                }
+            }
+            if (operators[operators.length-1] === '('){
+                operators.pop();
+            }
+            else{
+                throw Error("not opening bracket");
+            }
+            continue;
+        }
+        if (exp[i] === '*' || exp[i] === '/' || exp[i] === '+' || (exp[i] === '-' && temp.length > 0) || exp[i] === '^'){
             console.log("operator", temp);
-            expression.push(temp);
-            temp = "";
+            if (temp.length > 0){
+                expression.push(temp);
+                temp = "";
+            }
             if (expression.length >= 2){
-                if (operators.length > 0 && (operators[operators.length-1] === '*' || operators[operators.length-1] === '/')){
+                // same code over and over again but easier to read and understand because less things cluttered into boolean conditions
+                if (operators.length > 0 && operators[operators.length-1] === '^'){
+                    let one : string | undefined = expression.pop();
+                    let two : string | undefined = expression.pop();
+                    let op : string | undefined = operators.pop();
+                    if (one !== undefined && two !== undefined && op !== undefined){
+                        expression.push(two + " " + one + op);
+                    }
+                }
+                if (operators.length > 0 && (operators[operators.length-1] === '*' || operators[operators.length-1] === '/') && exp[i] !== '^'){
                     let one : string | undefined = expression.pop();
                     let two : string | undefined = expression.pop();
                     let op : string | undefined = operators.pop();
@@ -37,9 +125,7 @@ function Calculator() {
                         expression.push(one + " " + two + op);
                     }
                 }
-                if (operators.length > 0 && 
-                    (operators[operators.length-1] === '+' || operators[operators.length-1] === '-') && 
-                    exp[i] !== '*' && exp[i] !== '/') {
+                if (operators.length > 0 && (operators[operators.length-1] === '+' || operators[operators.length-1] === '-') && exp[i] !== '*' && exp[i] !== '/' && exp[i] !== '^') {
                     let one : string | undefined = expression.pop();
                     let two : string | undefined = expression.pop();
                     let op : string | undefined = operators.pop();
@@ -64,7 +150,15 @@ function Calculator() {
         counter++;
         if (counter >= 3000) {
             throw new Error("inf loop");
-        } 
+        }
+        if (operators.length > 0 && operators[operators.length-1] === '^') {
+            let one : string | undefined = expression.pop();
+            let two : string | undefined = expression.pop();
+            let op : string | undefined = operators.pop();
+            if (one !== undefined && two !== undefined && op !== undefined){
+                expression.push(two + " " + one + op);
+            }
+        }
         if (operators.length > 0 && (operators[operators.length-1] === '*' || operators[operators.length-1] === '/')){
             let one : string | undefined = expression.pop();
             let two : string | undefined = expression.pop();
@@ -86,13 +180,11 @@ function Calculator() {
     // after here, just need the postfix expression
     console.log("after while: ", expression, operators);
     let postfix: string | undefined = expression.pop();
-    let values: Array<number> = [],
-      tmp: string = "";
-    // problems with negative signs: if the negative sign is by itself and tmp length is equal to 0
+    let values: Array<number> = [], tmp: string = "";
     if (postfix !== undefined) {
         for (let i = 0; i < postfix.length; i++) {
             console.log("values: ", values);
-            if (values.length > 0 && (postfix[i] === "+" || (postfix[i] === "-" && (tmp.length > 0 || isNaN(parseInt(postfix[i+1])) || i + 1 === postfix.length)) || postfix[i] === "*" || postfix[i] === "/")) {
+            if (values.length > 0 && (postfix[i] === "+" || (postfix[i] === "-" && (tmp.length > 0 || isNaN(parseInt(postfix[i+1])) || i + 1 === postfix.length)) || postfix[i] === "*" || postfix[i] === "/" || postfix[i] === '^')) {
                 if (tmp.length > 0) {
                     values.push(parseFloat(tmp));
                     tmp = "";
@@ -114,6 +206,9 @@ function Calculator() {
                         case "/":
                             values.push(v1 / v2);
                             break;
+                        case '^':
+                            values.push(Math.pow(v2, v1));
+                            break;
                         default:
                             break;
                     }
@@ -123,9 +218,6 @@ function Calculator() {
                 values.push(parseFloat(tmp));
                 tmp = "";
             } else {
-                // if (postfix[i] === '-'){
-                //     console.log("add digit", parseInt(postfix[i+1]), isNaN(parseInt(postfix[i+1])), postfix[i+1]);
-                // }
                 tmp += postfix[i];
             }
         }
@@ -160,7 +252,7 @@ function Calculator() {
         <div id="answer">
           {expression.length > 0 &&
           !expression.match(/^[A-Za-z]+$/) &&
-          answer !== undefined
+          answer !== undefined && !isNaN(answer)
             ? "= " + answer
             : ""}
         </div>

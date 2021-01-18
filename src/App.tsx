8c1561ec1d5fdd1582ebc17a361629b4a9a780c8
cod1r <jasonho353@gmail.com>
2021-01-17 22:00:46 -0600

    used react context for the first time and added toggle button for background change

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import React, { useState } from 'react';
+import { themes } from './contexts/themes';
 import './App.css';
 import Calculator from "./components/Calculator";
+const Theme = React.createContext(themes.dark);
 function App() {
+  const [theme, setTheme] = useState(themes.light);
   return (
-      <Calculator></Calculator>
+    <Theme.Provider value={theme}>
+        <Theme.Consumer>
+        {theme =>
+            <div style={{backgroundColor: theme, height: '100%'}}>
+              <button onClick={()=>setTheme(theme === themes.light ? themes.dark:themes.light)}>Toggle</button>
+              <Calculator></Calculator>
+            </div>
+        }
+        </Theme.Consumer>
+    </Theme.Provider>
   );
 }
 
